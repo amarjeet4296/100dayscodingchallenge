5d6a91c13ca44fa2118ae3d89e6e536927b66274
amarjeet krishnan <63232182+amarjeet4296@users.noreply.github.com>
2021-02-27 21:23:50 +0530

    Day 34

---
+++ Diagonal_traversal_in_BST.py
@@ -0,0 +1,44 @@
+from collections import deque
+class Node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+def diagonalPrint(root):
+
+    q = deque()
+    dummy = Node(-1)
+
+    while root:
+        q.append(root)
+        root = root.right
+
+    q.append(dummy)
+
+    while len(q) != 1:
+        front = q.popleft()
+        if front != dummy:
+            print(front.data, end=' ')
+            node = front.left
+            while node:
+                q.append(node)
+                node = node.right
+
+        else:
+            q.append(dummy)
+            print()
+
+
+if __name__ == '__main__':
+
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.right.left = Node(5)
+    root.right.right = Node(6)
+    root.right.left.left = Node(7)
+    root.right.left.right = Node(8)
+
+    diagonalPrint(root)
